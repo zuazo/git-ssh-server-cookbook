4808dde194db780a9ea71aa7c1c048a45bc70c11
Xabier de Zuazo <xabier@onddo.com>
2013-05-31 20:54:46 +0200

    metadata file completed with recipe, resources and attributes description

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
-name             "git-repo"
-maintainer       "Onddo Labs, Sl."
-maintainer_email "team@onddo.com"
-license          "Apache 2.0"
-description      "Installs/Configures git-repo"
+name             'git-repo'
+maintainer       'Onddo Labs, Sl.'
+maintainer_email 'team@onddo.com'
+license          'Apache 2.0'
+description      'Configures a basic git repository container to be accesed through ssh'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          '0.0.1'
 
 depends 'git'
 
@@ -12,5 +12,45 @@ depends 'git'
   supports os
 end
 
-supports "mac_os_x", ">= 10.6.0"
+supports 'mac_os_x', '>= 10.6.0'
+
+recipe 'git-repo::default', 'Configures a git repository container'
+
+provides 'git_repo_bare[name]'
+provides 'git_repo_ssh_key[keyname]'
+
+attribute 'git-repo/base_path',
+  :display_name => 'Base path',
+  :description => 'Git server base path',
+  :calculated => true,
+  :type => 'string',
+  :required => 'optional'
+
+attribute 'git-repo/user',
+  :display_name => 'System user',
+  :description => 'System user used to manage the repositories',
+  :type => 'string',
+  :required => 'optional',
+  :default => 'git'
+
+attribute 'git-repo/group',
+  :display_name => 'System group',
+  :description => 'System group used to manage the repositories',
+  :type => 'string',
+  :required => 'optional',
+  :default => 'git'
+
+attribute 'git-repo/shell',
+  :display_name => 'Git user shell',
+  :description => 'Git user shell',
+  :type => 'string',
+  :required => 'optional',
+  :default => '/usr/bin/git-shell'
+
+attribute 'git-repo/hostname',
+  :display_name => 'Machine hostname',
+  :description => 'Machine hostname, used only in the README template',
+  :calculated => true,
+  :type => 'string',
+  :required => 'recommended'
 
