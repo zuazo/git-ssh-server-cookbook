d6e0c5619c44f0722a610dc699c3dee85c3f89b1
Xabier de Zuazo <xabier@onddo.com>
2013-06-08 15:33:51 +0200

    release v0.1.0

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# CHANGELOG for dovecot
+
+This file is used to list changes made in each version of dovecot.
+
+## 0.1.0:
+
+* Initial release of `git-ssh-server`
+
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'team@onddo.com'
 license          'Apache 2.0'
 description      'Set up a basic git repository container to be accesed via ssh'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.1'
+version          '0.1.0'
 
 depends 'git'
 
