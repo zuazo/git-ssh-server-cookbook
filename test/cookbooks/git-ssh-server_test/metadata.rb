4d9f8886e91803cca426c1c5a75d3f446276e693
Xabier de Zuazo <xabier@onddo.com>
2014-11-16 17:18:34 +0100

    move test/kitchen/cookbooks to test/cookbooks

--- test/cookbooks/git-ssh-server_test/metadata.rb
+++ test/cookbooks/git-ssh-server_test/metadata.rb
@@ -2,7 +2,8 @@ name             'git-ssh-server_test'
 maintainer       'Onddo Labs, Sl.'
 maintainer_email 'team@onddo.com'
 license          'Apache 2.0'
-description      'Installs/Configures git-ssh-server_test'
+description 'This cookbook is used with test-kitchen to test the parent, '\
+            'git-ssh-server cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
