e37cfde3b8aea1a1a94a685f2774c2e2a70d9a7f
Xabier de Zuazo <xabier@zuazo.org>
2015-09-09 12:31:58 +0200

    Fix foodcritic offense

--- providers/bare.rb
+++ providers/bare.rb
@@ -31,13 +31,13 @@ action :create do
   name = new_resource.name.sub(/[.]git$/, '')
   base_path = new_resource.base_path || node['git-ssh-server']['base_path']
 
-  unless shell_out(
+  unless shell_out( # ~FC023
            "git rev-parse --resolve-git-dir '#{base_path}/#{name}.git' "\
            '> /dev/null 2>&1',
            {
              :user => node['git-ssh-server']['user'],
              :group => node['git-ssh-server']['group']
-           }).status.success? # ~FC023
+           }).status.success?
     converge_by("Create #{new_resource}") do
       execute "git init --bare #{name}" do
         command "git init --bare '#{base_path}/#{name}.git'"
