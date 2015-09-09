361304eaa37316045f62e1d8d92fd2e6ca129545
Xabier de Zuazo <xabier@zuazo.org>
2015-09-09 12:16:59 +0200

    Add license headers to code files

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,29 @@
+# encoding: UTF-8
+#
+# Cookbook Name:: git-ssh-server
+# Attributes:: default
+# Author:: Xabier de Zuazo (<xabier@zuazo.org>)
+# Copyright:: Copyright (c) 2015 Xabier de Zuazo
+# Copyright:: Copyright (c) 2013-2014 Onddo Labs, SL.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-default['git-ssh-server']['base_path'] = nil # get from node['git']['server']['base_path']
+# Get from `node['git']['server']['base_path']`
+default['git-ssh-server']['base_path'] = nil
+
 default['git-ssh-server']['user'] = 'git'
 default['git-ssh-server']['group'] = 'git'
 default['git-ssh-server']['shell'] = '/usr/bin/git-shell'
 default['git-ssh-server']['hostname'] = node['fqdn']
-
