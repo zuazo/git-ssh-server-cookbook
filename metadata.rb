d35aa6e88cbadfea715c9523793c37e316693b0e
Xabier de Zuazo <xabier@zuazo.org>
2015-09-10 07:06:25 +0200

    version bumped to 1.1.0 WiP

--- metadata.rb
+++ metadata.rb
@@ -25,7 +25,7 @@ maintainer_email 'xabier@zuazo.org'
 license 'Apache 2.0'
 description 'Set up a basic git repository container to be accesed via SSH.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.0.0'
+version '1.1.0' # WiP
 
 if respond_to?(:source_url)
   source_url "https://github.com/zuazo/#{name}-cookbook"
